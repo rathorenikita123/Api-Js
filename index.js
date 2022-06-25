dc18b12f41f48858edf20b6843d7f148dfc18957
rathorenikita123e <nikitarathore271201@gmail.com>
2022-06-25 22:22:27 +0530

    changed some files

--- index.js
+++ index.js
@@ -3,26 +3,25 @@ async function loadIntoTable(url, table) {
     const tableBody = table.querySelector('tbody');
     const response = await fetch(url);
     const {users, rows } = await response.json();
-    
-    tableHead.innerHTML = '<tr></tr>';
-    tableHead.innerHTML = '';
 
     for (const userText of users) {
         const userElement = document.createElement('th');
         userElement.textContent = userText;
-        tableHead.querySelector('tr').appendChild(userElement);
+        tableHead.appendChild(userElement);    
     }
 
     for (const row of rows) {
         const rowElement = document.createElement('tr');
 
-        for (const cell of row) {
+        for (const cellText of row) {
             const cellElement = document.createElement('td');
-            cellElement.textContent = cell;
+            cellElement.textContent = cellText;
             rowElement.appendChild(cellElement);
         }
-        tableBody.appendChild(rowElement);
+        tableBody.appendChild(rowElement);  
     }
 }
 
 loadIntoTable('./database.json', document.querySelector('table'));
+
+
